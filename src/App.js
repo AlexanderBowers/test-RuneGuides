c27d6bc30b89f1cca9938214da8eaf92d23ba394
AlexanderBowers <7319abowers@gmail.com>
2021-05-14 18:51:53 -0700

    added paragraph

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>welcome to RuneGuides</h1>
+      <p>The purpose of this site is to condense several runescape guides into one package. I hope
+        to include links to all sorts of guides whether they be videos, images, or on the wiki.
+      </p>
     </div>
   );
 }
