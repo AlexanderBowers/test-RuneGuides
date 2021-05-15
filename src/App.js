ebb33bd6b6f1f0fb73550b549df5bd082e070006
AlexanderBowers <7319abowers@gmail.com>
2021-05-14 18:57:24 -0700

    created link to wiki

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <p>The purpose of this site is to condense several runescape guides into one package. I hope
         to include links to all sorts of guides whether they be videos, images, or on the wiki.
       </p>
+      Link to wiki: <a href="runescape.wiki">RuneScape Wiki</a>
     </div>
   );
 }
